3309645ee76c05074b07c15640e598f87deb7d1e
deepjyotighosh33-droid <deepjyotighosh33@gmail.com>
2026-02-04 11:17:04 +0530

    Fresh Arrivals

---
+++ All python codes/Chapter 11/02_multiple_inheritance.py
@@ -0,0 +1,22 @@
+class Employee:               # Base Class
+    company = "ITC"
+    name = "Default Name"
+    def show(self):
+        print(f"The name of the Employee is {self.name} and the company is {self.company}")
+
+class coder:                  # Base Class
+    language = "Python"
+    def printLanguages(self):
+        print(f"Out of all the languages here is your language: {self.language}")
+
+class Programmer(Employee, coder):    # Child Class
+    company = "ITC Infotech"
+    def showLanguage(self):
+        print(f"The name is {self.name} and he is good with {self.language} language")
+
+a = Employee()
+b = Programmer()
+
+b.show()
+b.printLanguages()
+b.showLanguage()
